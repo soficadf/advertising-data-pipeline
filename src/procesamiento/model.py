8f295aa8d58f3d9cef59c774ea1c92e14efcad58
Sofia <s.cadenas@alumnos.upm.es>
2026-08-13 00:32:44 +0200

    fix: simuladores

--- src/procesamiento/model.py
+++ src/procesamiento/model.py
@@ -1,32 +1,34 @@
-import enum
+
+from enum import Enum
+
 
 #Nombre de las carpetas del datalake
-class Layers (enum):
+class Layers (Enum):
     LANDING="landing"
     BRONZE= "bronze"
     SILVER="silver"
     GOLD="gold"
 
-class DatasetsLanding(enum):
+class DatasetsLanding(Enum):
     VENTAS="ventas"
     META="meta_ads"
 
-class DatasetsBronze(enum):
+class DatasetsBronze(Enum):
     VENTAS="ventas"
     META="meta_ads"
 
-class DatasetsSilver(enum):
+class DatasetsSilver(Enum):
     VENTAS="ventas_diarias"
     META="meta_ads"
     SPEND="ad_spend"
 
-class DatasetsGold(enum):
+class DatasetsGold(Enum):
     REACH="gold_demographic_reach"
     DAILY_METRICS="gold_ad_daily_metrics"
     SATURATION="gold_saturation_curve"
     TOTAL_METRICS="gold_ad_total_metrics"
 
-class TablesSQl(enum):
+class TablesSQl(Enum):
     SPEND="daily_spend"
     PRODUCT_MAP="ad_product_mapping"
     PRODUCTS="products"
